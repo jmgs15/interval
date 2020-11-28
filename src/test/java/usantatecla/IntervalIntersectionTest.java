83c677b64e9afdd78970f0f28615d6822b28c537
Juanma <jmgs15@github.com>
2020-11-28 23:38:23 +0100

    Added tests of ClosedIntervals

--- src/test/java/usantatecla/IntervalIntersectionTest.java
+++ src/test/java/usantatecla/IntervalIntersectionTest.java
@@ -18,26 +18,29 @@ public class IntervalIntersectionTest {
     private Point right;
     private Point leftToCompare;
     private Point rightToCompare;
-    private boolean expectedIsIntersected;
+    private boolean expectedIsIntersectedOpen;
+    private boolean expectedIsIntersectedClose;
 
-    public IntervalIntersectionTest(String name, Point left, Point right, Point leftToCompare, Point rightToCompare, boolean expectedIsIntersected) {
+    public IntervalIntersectionTest(String name, Point left, Point right, Point leftToCompare, Point rightToCompare,
+                                    boolean expectedIsIntersectedOpen, boolean expectedIsIntersectedClose) {
         this.name = name;
         this.left = left;
         this.right = right;
         this.leftToCompare = leftToCompare;
         this.rightToCompare = rightToCompare;
-        this.expectedIsIntersected = expectedIsIntersected;
+        this.expectedIsIntersectedOpen = expectedIsIntersectedOpen;
+        this.expectedIsIntersectedClose = expectedIsIntersectedClose;
     }
 
     @Parameterized.Parameters(name = "(Test {0}: Interval({1}, {2}) | IntervalToCompare({3}, {4}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                { "This Interval contains leftToCompare", point(1), point(5), point(2), point(8), true },
-                { "This Interval contains rightToCompare", point(1), point(5), point(-1), point(3), true },
-                { "This Interval is in IntervalToCompare", point(3), point(5), point(1), point(8), true },
-                { "This Interval is in IntervalToCompare with same left", point(3), point(5), point(3), point(8), true },
-                { "This Interval's left has same IntervalToCompare's right", point(5), point(8), point(3), point(5), false },
-                { "This Interval's left and right has same IntervalToCompare's left and right", point(5), point(8), point(5), point(8), true }
+                { "This Interval contains leftToCompare", point(1), point(5), point(2), point(8), true, true },
+                { "This Interval contains rightToCompare", point(1), point(5), point(-1), point(3), true, true },
+                { "This Interval is in IntervalToCompare", point(3), point(5), point(1), point(8), true, true },
+                { "This Interval is in IntervalToCompare with same left", point(3), point(5), point(3), point(8), true, true },
+                { "This Interval's left has same IntervalToCompare's right", point(5), point(8), point(3), point(5), false, true },
+                { "This Interval's left and right has same IntervalToCompare's left and right", point(5), point(8), point(5), point(8), true, true }
         });
     }
 
@@ -45,7 +48,14 @@ public class IntervalIntersectionTest {
     public void givenIntervalOpenOpenTestIsIntersected() {
         Interval interval = new IntervalBuilder().open(left.getEquals()).open(right.getEquals()).build();
         Interval intervalToCompare = new IntervalBuilder().open(leftToCompare.getEquals()).open(rightToCompare.getEquals()).build();
-        assertThat(interval.isIntersected(intervalToCompare), is(expectedIsIntersected));
+        assertThat(interval.isIntersected(intervalToCompare), is(expectedIsIntersectedOpen));
+    }
+
+    @Test
+    public void givenIntervalCloseCloseTestIsIntersected() {
+        Interval interval = new IntervalBuilder().closed(left.getEquals()).closed(right.getEquals()).build();
+        Interval intervalToCompare = new IntervalBuilder().open(leftToCompare.getEquals()).open(rightToCompare.getEquals()).build();
+        assertThat(interval.isIntersected(intervalToCompare), is(expectedIsIntersectedClose));
     }
 
     private static Point point(double value) {
